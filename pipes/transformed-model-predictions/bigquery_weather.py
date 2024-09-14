67e696e036cecac25d9464c1d85c624e36ef1e90
OE-ITHS <oscar.eriksson@iths.se>
2024-09-14 19:15:12 +0200

    changed data fetch logic to instead use sql query code to query the bigquery view

--- pipes/transformed-model-predictions/bigquery_weather.py
+++ pipes/transformed-model-predictions/bigquery_weather.py
@@ -14,18 +14,27 @@ def fetch_bq_data() -> pd.DataFrame:
     bigquery_client = bigquery.Client()
     project_id = 'acquired-sound-433108-c6'
     dataset_id = 'weather_data'
-    table_id = 'weather_transformed'
+    view_id = 'weather_transformed'
+
+    query = f'SELECT * FROM `{project_id}.{dataset_id}.{view_id}` ORDER BY dt DESC LIMIT 4'
 
     try:
-        # Creates a pointer (ish) object to the dataset using the project- and dataset id
-        dataset_ref = bigquery.DatasetReference(project_id, dataset_id)
-        # Creates a pointer (ish) object to the table (on this case a view) using the previous dataset_ref
-        table_ref = dataset_ref.table(table_id)
-        # Creates a table object with the same data as the actual table on bigquery using the previous table_ref
-        table = bigquery_client.get_table(table_ref)
 
-        # Uses the previously initalized bigquery client to list all the rows of the table object and puts them in a (pandas) dataframe.
-        df = bigquery_client.list_rows(table).to_dataframe()
+        query_job = bigquery_client.query(query)
+
+        results = query_job.result()
+
+        df = results.to_dataframe()
+        
+        # # Creates a pointer (ish) object to the dataset using the project- and dataset id
+        # dataset_ref = bigquery.DatasetReference(project_id, dataset_id)
+        # # Creates a pointer (ish) object to the table (on this case a view) using the previous dataset_ref
+        # table_ref = dataset_ref.table(view_id)
+        # # Creates a table object with the same data as the actual table on bigquery using the previous table_ref
+        # table = bigquery_client.get_table(table_ref)
+
+        # # Uses the previously initalized bigquery client to list all the rows of the table object and puts them in a (pandas) dataframe.
+        # df = bigquery_client.list_rows(table).to_dataframe()
 
         return df
     except Exception as e:
