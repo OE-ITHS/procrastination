3560867a76551671bb0d904e0f746eff781257c6
OE-ITHS <oscar.eriksson@iths.se>
2024-09-14 22:12:18 +0200

    reversed .shift() direction due to inversed data row order

--- pipes/transformed-model-predictions/transform.py
+++ pipes/transformed-model-predictions/transform.py
@@ -5,15 +5,15 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     Temporary function meant for testing transformed-model-predictions without having to wait for weather-transformed-view.sql to be finished and deployed
     '''
 
-    # Generate time-based features
+    # Generate time-based features.
     df['hour'] = pd.to_datetime(df['dt'], unit='s').dt.hour
     df['month'] = pd.to_datetime(df['dt'], unit='s').dt.month
     
-    # Generate lag features (e.g., temperature 1 hour ago)
-    df['temp_lag_1'] = df['temp'].shift(1)
-    df['temp_lag_3'] = df['temp'].shift(3)
+    # Generate lag features (e.g., temperature 1 hour ago) (reversed the shift direction because I sorted data in opposite direction from training data).
+    df['temp_lag_1'] = df['temp'].shift(-1)
+    df['temp_lag_3'] = df['temp'].shift(-3)
 
-    # Feature selection (yes I'm copying from Esoon's github)
+    # Feature selection (yes I'm copying from Esoon's github).
     parsed_df = df[['hour', 'month', 'temp', 'humidity', 'pressure', 'temp_lag_1', 'temp_lag_3', 'dt']]
 
     parsed_df = parsed_df.dropna()
