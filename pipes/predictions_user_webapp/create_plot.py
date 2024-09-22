ce2aff19dfca0d0e7c078fcdf99bff68cbf2457c
OE-ITHS <oscar.eriksson@iths.se>
2024-09-22 16:41:47 +0200

    converted temperature to celsius and unix timestamp to datetime

--- pipes/predictions_user_webapp/create_plot.py
+++ pipes/predictions_user_webapp/create_plot.py
@@ -1,14 +1,15 @@
 import urllib.parse, io, base64
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 def generate_plot(joined_df):
     
     limited_df = joined_df.iloc[:50]
 
-    X = limited_df['dt']
+    X = [datetime.fromtimestamp(dt/1000).strftime('%b %d %H:%M') for dt in limited_df['dt']]
 
-    Y1 = limited_df['oxygen_d1']
-    Y2 = limited_df['temp']
+    Y1 = [o for o in limited_df['oxygen_d1']]
+    Y2 = [(t-273.15) for t in limited_df['temp']]
 
     fig = plt.figure()
     fig.set_facecolor('#212529')
@@ -19,12 +20,12 @@ def generate_plot(joined_df):
     ax1.tick_params(colors='white', which='both')
     ax1.set_ylabel('Oxygen Content (ml/l)', color='b')
     ax1.tick_params(axis='y', colors='b')
-    ax1.grid(True, linestyle='--')
+    ax1.grid()
 
     ax2.plot(X, Y2, 'o-r')
     ax2.yaxis.tick_right()
     ax2.tick_params(colors='white', which='both')
-    ax2.set_ylabel('Temperature (C)', color='r')
+    ax2.set_ylabel('Temperature (\N{DEGREE SIGN}C)', color='r')
     ax2.yaxis.set_label_position('right')
     ax2.tick_params(axis='y', colors='r')
 
